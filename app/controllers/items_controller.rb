17e19dd85e8c096f9c633d3ba57a0a99772feb91
tomykaira <tomykaira@gmail.com>
2013-11-23 22:07:39 +0900

    Handle errors from buy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class ItemsController < ApplicationController
   before_filter :authenticate_customer!, only: [:buy]
 
@@ -10,6 +11,10 @@ class ItemsController < ApplicationController
   def buy
     item = Item.find(params[:id])
     current_customer.buy(item)
-    redirect_to :index
+    redirect_to items_path, notice: "#{item.name}を購入しました"
+  rescue Customer::NoWebPayAccountError
+    redirect_to edit_customer_registration_path, notice: 'カード情報が未登録です'
+  rescue Customer::ChargeFailed => e
+    redirect_to items_path, notice: "支払いできませんでした (#{e.message})"
   end
 end

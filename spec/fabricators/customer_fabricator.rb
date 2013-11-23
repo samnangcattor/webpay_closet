fb7bc8817398d3c9f7602f727f2f34e1d512f74d
tomykaira <tomykaira@gmail.com>
2013-11-23 20:11:05 +0900

    Fix broken customer's fabricator

--- spec/fabricators/customer_fabricator.rb
+++ spec/fabricators/customer_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:customer) do
+  name { Faker::Name.name }
   email { |attrs| "#{attrs[:name].parameterize}@example.com" }
   password { 'cusomerPass' }
-  name { Faker::Name.name }
   address { Faker::Address.street_address }
   disabled false
 end

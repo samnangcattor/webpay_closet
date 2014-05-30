e9bd8f17e6e61cf5eb5e05cbdcf723e1b7ddeeb5
gogotanaka <qlli.illb@gmail.com>
2014-05-30 21:10:17 +0900

    Add spec of webhook_controller

--- spec/fabricators/recursion_fabricator.rb
+++ spec/fabricators/recursion_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:recursion) do
   customer            nil
   item                nil
-  webpay_recursion_id
-  period              "month"
+  webpay_recursion_id 'rec_XXXXXXXXX'
+  period              'month'
 end
